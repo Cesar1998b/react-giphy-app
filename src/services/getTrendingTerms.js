f5f5c6fc5c11b2c8b3f3e0f5e03616c341f996fa
Cesar David Botina <cesarbotina98@gmail.com>
2021-09-21 20:51:37 -0500

    get trends of gifs using api

---
+++ src/services/getTrendingTerms.js
@@ -0,0 +1,16 @@
+import { API_KEY, API_URL } from "./settings";
+
+const fromApiResponseTrends = apiResponse => {
+    const {data = []} = apiResponse
+    return data
+}
+
+
+export default function getTrendingTerms() {
+    const url =
+    `${API_URL}/trending/searches?api_key=${API_KEY}`;
+    
+    return fetch(url)
+        .then(res => res.json())
+        .then(fromApiResponseTrends)
+  };
