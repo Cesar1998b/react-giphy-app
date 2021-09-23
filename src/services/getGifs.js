4670c4bc6c02e9e7d85aadad499163a33f9bbc64
Cesar David Botina <cesarbotina98@gmail.com>
2021-09-23 01:10:57 -0500

    modify form service

--- src/services/getGifs.js
+++ src/services/getGifs.js
@@ -1,17 +1,25 @@
-import { API_KEY, API_URL } from "./settings";
+import {API_KEY, API_URL} from './settings'
 
-export const getGifs = async (category) => {
-    const url =
-    `${API_URL}/gifs/search?q=${encodeURI(category)}&limit=20&api_key=${API_KEY}`;
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
+const fromApiResponseToGifs = apiResponse => {
+  const {data = []} = apiResponse
+  if (Array.isArray(data)) {
+    const gifs = data.map(image => {
+      const {images, title, id} = image
+      const { url } = images.downsized_medium
+      return { title, id, url }
+    })
+    return gifs
+  }
+  return []
+}
 
-    return gifs;
-  };
+export default function getGifs({
+  limit = 15,
+  keyword = "Cats"
+} = {}) {
+  const apiURL = `${API_URL}/gifs/search?api_key=${API_KEY}&q=${keyword}&limit=${limit}&lang=en`
+
+  return fetch(apiURL)
+    .then((res) => res.json())
+    .then(fromApiResponseToGifs)
+}
