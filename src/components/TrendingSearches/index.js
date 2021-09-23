e83db90ca00f2dd119791a83d340abe2b435dc53
Cesar David Botina <cesarbotina98@gmail.com>
2021-09-22 22:46:35 -0500

    add react lazy with suspense

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,20 +1,16 @@
-import React, { useEffect, useState } from "react";
-import getTrendingTerms from "services/getTrendingTerms";
-import Category from "../Category";
-import useNearScreen from "hooks/useNearScreen";
+import React, {Suspense} from "react";
+import useNearScreen from "hooks/useNearScreen"; 
 
-function TrendingSearches() {
-  const [trends, setTrends] = useState([]);
-
-  useEffect(function () {
-    getTrendingTerms().then(setTrends);
-  }, []);
-
-  return <Category name="Trends" options={trends} />;
-}
+const TrendingSearches = React.lazy(
+  () => import('./TrendingSearches')
+)
 
 export default function LazyTrending() {
   const { isNearScreen, fromRef } = useNearScreen();
 
-  return <div ref={fromRef}>{isNearScreen ? <TrendingSearches /> : null}</div>;
+  return <div ref={fromRef}>
+      <Suspense fallback={null}>
+        {isNearScreen ? <TrendingSearches /> : null}
+      </Suspense>
+    </div>;
 }
