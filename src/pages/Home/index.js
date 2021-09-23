8c99521e130e19a92a6f13e3449013ba678f297b
Cesar David Botina <cesarbotina98@gmail.com>
2021-09-23 01:11:17 -0500

    change name of components used

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,27 @@
-import React, { useState } from 'react';
-import SearchResults from '../SearchResults/index';
-import GifCollectionItem from 'components/ListOfGifs';
+import React from 'react';
+import SearchForm from '../../components/SearchForm/index';
 import TrendingSearches from 'components/TrendingSearches';
 import { useFetchGifs } from 'hooks/useFetchGifs';
-import "./Home.css";
+import ListOfGifs from 'components/ListOfGifs';
 
 export default function Home() {
-  const [keyword, setKeyword] = useState("");
   const { loading, gifs } = useFetchGifs();
 
   return (
     <section>
-      <SearchResults keyword={keyword} setKeyword={setKeyword} />
-      <h3 className="title">Last Search</h3>
-      {loading && <p className="animate__animated animate__flash">Loading</p>}
-      <div className="App-main">
-        <div className="card-grid">
-          {gifs.map((img) => (
-            <GifCollectionItem key={img.id} {...img} />
-          ))}
-        </div>
-        <div className="App-category">
-          <TrendingSearches />
+      <header>
+        <SearchForm />
+      </header>
+      <div className="App-wrapper">
+        <div className="App-main">
+          <div className="App-results">
+            <h3 className="title">última búsqueda</h3>
+            {loading && <p className="animate__animated animate__flash">Loading</p>}
+            <ListOfGifs gifs={gifs}/>
+          </div>
+          <div className="App-category">
+            <TrendingSearches />
+          </div>
         </div>
       </div>
     </section>
