68aefcdcdf4e2871c98ec6a073f6bec4f8705a7b
Cesar David Botina <cesarbotina98@gmail.com>
2021-09-25 01:21:20 -0500

    add offset

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -2,8 +2,13 @@ import { useState, useEffect,useContext} from "react";
 import  getGifs  from "../services/getGifs";
 import GifContexts from '../context/GifContexts'
 
+const INITIAL_PAGE = 0
+
 export function useFetchGifs({keyword} = {keyword : null}) {
   const [loading, setLoading] = useState(false)
+  const [loadingNextPage, setLoadingNextPage] = useState(false)
+
+  const [page, setPage] = useState(INITIAL_PAGE)
   const {gifs, setGifs} = useContext(GifContexts)
 
   const keywordToUse = keyword || localStorage.getItem('lastKeyword') || 'gatitos'
@@ -19,5 +24,17 @@ export function useFetchGifs({keyword} = {keyword : null}) {
     });
   }, [keyword, keywordToUse, setGifs]);
 
-  return {loading,gifs};
+  useEffect(function () {
+    if (page === INITIAL_PAGE) return
+
+    setLoadingNextPage(true)
+
+    getGifs({ keyword: keywordToUse, page })
+      .then(nextGifs => {
+        setGifs(prevGifs => prevGifs.concat(nextGifs))
+        setLoadingNextPage(false)
+      })
+  }, [keywordToUse, page, setGifs])
+
+  return {loading, loadingNextPage, gifs, setPage};
 };
