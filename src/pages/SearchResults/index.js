6bfe925a20892c8fe5f5dfba252a5595c93efb2d
Cesar David Botina <cesarbotina98@gmail.com>
2021-09-25 01:22:50 -0500

    add a useCallback and visor for detect infinite scroll

--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useRef, useCallback, useEffect} from "react";
 import ListOfGifs from "components/ListOfGifs";
 import SearchForm from "components/SearchForm";
 import { useFetchGifs } from "hooks/useFetchGifs";
+import useNearScreen from "hooks/useNearScreen";
+import debounce from 'just-debounce-it'
 
 export default function SearchResults({ params }) {
   const { keyword } = params;
-  const { loading, gifs } = useFetchGifs({ keyword });
+  const { loading, gifs, setPage } = useFetchGifs({ keyword });
+  
+  const externalRef = useRef()
+  const {isNearScreen} = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false
+  })
 
-  const title = gifs ? `${gifs.length} resultados de ${keyword}` : "";
+  const title = gifs ? `${gifs.length} resultados de ${keyword}` : ''
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const debounceHandleNextPage = useCallback(debounce(
+    () => setPage(prevPage => prevPage + 1), 200
+  ), [setPage])
+
+  useEffect(function () {
+    if (isNearScreen) debounceHandleNextPage()
+  }, [debounceHandleNextPage, isNearScreen])
+
   return (
     <>
       {loading ? (
@@ -24,6 +42,7 @@ export default function SearchResults({ params }) {
           <div className="App-wrapper">
             <h3 className="App-title">{decodeURI(keyword)}</h3>
             <ListOfGifs gifs={gifs} />
+            <div id="visor" ref={externalRef}></div>
           </div>
         </>
       )}
